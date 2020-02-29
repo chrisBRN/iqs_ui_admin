ea3a5d11befb9a6d0249e9de54c69ecb337a9e08
Chris <chriskbrown@hotmail.co.uk>
2020-02-29 10:56:50 +0000

    Login Page Largely Done

--- src/shared/components/login-card/LoginCard.tsx
+++ src/shared/components/login-card/LoginCard.tsx
@@ -2,26 +2,65 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import InputField from '../input/InputField';
+import Logo from '../logo/Logo';
+import ExternalLink from '../../helpers/ExternalLink';
 
 export default function LoginCard() {
 
     return (
-        <div>                                           
-            <h1 className="welcome-headline">WELCOME</h1>  
-            
+        <div className="brand-section">
+
+            <h1 className="brandname-h1">
+                <ExternalLink 
+                    endpoint={"https://www.coderKai.com/"} 
+                    anchorText={"CoderKai"} 
+                />                 
+            </h1>
+
+            <div className="mini-logos">
+               
+                <div className="iqs">
+                    <Logo /> 
+                    <ExternalLink 
+                        endpoint={"/home"} 
+                        anchorText={"iQuestions"} 
+                    />                 
+                </div>  
+
+                <div className="algo-cash">
+                    <h2>&#x20BF;</h2>
+                    <ExternalLink 
+                        endpoint={"https://www.algocash.com/"} 
+                        anchorText={"algoCASH"} 
+                    />                 
+                </div>                    
+                              
+            </div>
+
+            <hr></hr>
+            <h2>Login</h2>
+
             <form className="login-card-form">
 
-                <InputField label={"Username"}/>
-                <InputField type={"password"} label={"Password"}/>                   
+                <InputField label={"Username"} />
+                <InputField type={"password"} label={"Password"} />
 
                 <div className="sign-in-button-container">
-                    <button className="sign-in-button">Sign In</button>
+                    <button className="sign-in-button">Login</button>
                 </div>
 
-                {/* 
+                <div className="link-container">
+                    <Link className="additional-info-mid-print" to="/todo">Forgot password?</Link>
+                </div>
 
-                <Link className="additional-info-mid-print" to="/todo">Forgot password?</Link>
-                <Link className="additional-info-mid-print" to="/todo">Candidate? Login Here</Link> */}
+                <div className="link-container candidate">
+                    <Link className="additional-info-mid-print" to="/todo">Candidate? Login Here</Link>
+                </div>
+                
+                
+                
+                
+                
 
             </form>
 
