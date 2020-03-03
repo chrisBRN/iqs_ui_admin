a32d8fbe4343d6e439b714cbb05911db79811eb9
Chris <chriskbrown@hotmail.co.uk>
2020-03-03 12:40:10 +0000

    Updated Login Page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,66 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-
-import Logo from '../../shared/components/logo/Logo';
-import Hero from '../../shared/components/hero-image/Hero';
 import LoginCard from '../../shared/components/login-card/LoginCard'
-import BrandsCard from '../../shared/components/brands-card/BrandsCard';
-
 import ExternalLink from '../../shared/helpers/ExternalLink';
 
 import '../../shared/styling/global-styling.scss';
 import './LoginPage.scss';
-import '../../shared/components/brands-card/BrandsCard.scss';
 import '../../shared/components/login-card/LoginCard.scss';
 
-
-export default function Login() {     
+export default function Login() {
 
     return (
         <div className="login-page">
 
-            <div className="left-side">
-
-                <Link to="/home">
-                    <div className="logo">
-                        <Logo />
-                    </div>
-                </Link>
-
-                <div className="hero">
-                    <Hero />
-                    <div className="small-print">
-                        <ExternalLink 
-                            endpoint={"https://www.freepik.com/katemangostar"} 
-                            anchorText={"Original Source Image by Kate Mangostar @ Freepik.com"} 
-                        />
-                    </div>
-                </div>
-
+            <div className="login-card-container">  
+                <LoginCard />    
             </div>
 
-            <div className="right-side">
-
-                <div className="inner-box">
-
-                    <BrandsCard />
-                    <LoginCard />
-                
-
-                    
-
-                </div>
-
-                <div className="copyright-notice-container">
-                    <div className="small-print">    
-                        <ExternalLink 
-                            endpoint={"https://github.com/chrisBRN"} 
-                            anchorText={"Copyright © ChrisBRN " + new Date().getFullYear()}                     
-                        />
-                    </div>
-                </div>
-
+            <div className="copyright-notice-container">
                
+                <ExternalLink
+                    endpoint={"https://github.com/chrisBRN"}
+                    anchorText={"Copyright © ChrisBRN " + new Date().getFullYear()}
+                />                            
 
             </div>
 
--- src/shared/components/login-card/LoginCard.tsx
+++ src/shared/components/login-card/LoginCard.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import InputField from '../input/InputField';
-
+import ExternalLink from '../../helpers/ExternalLink';
 
 export default function LoginCard() {
 
     return (
-        <div className="login-card">
-         
-            <h2>Sign In</h2>
+        <div className="login-card">    
+
+            <div className="main-brand">
+                <ExternalLink 
+                    endpoint={"https://www.coderKai.com/"} 
+                    anchorText={"CoderKai"} 
+                />                 
+            </div>    
 
             <form className="login-card-form">
 
@@ -22,15 +27,15 @@ export default function LoginCard() {
 
                 <div className="link-container privacy-statement">
                     <Link className="small" to="/todo">By clicking Login, you agree to our Terms and have read and acknowledge our Privacy Statement.</Link>
+                </div>     
+
+                <div className="link-container forgot-password">
+                    <Link className="additional-info-mid-print" to="/todo">Forgot password?</Link>
                 </div>        
 
                 <div className="link-container candidate">
                     <Link className="additional-info-mid-print" to="/todo">Candidate? Login Here</Link>
                 </div>
-
-                <div className="link-container">
-                    <Link className="additional-info-mid-print" to="/todo">Forgot password?</Link>
-                </div>     
 
             </form>
 
